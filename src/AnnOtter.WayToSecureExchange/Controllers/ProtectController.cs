7909c7b64c6c858644b16775a6f8260c3abe207c
Daniel Scherrer <daniel.scherrer@webjoda.ch>
2023-12-03 17:32:11 +0100

    feat(#12): add protect url feature with documentation

---
+++ src/AnnOtter.WayToSecureExchange/Controllers/ProtectController.cs
@@ -0,0 +1,37 @@
+﻿using AnnOtter.WayToSecureExchange.Configuration.AppSettings;
+using AnnOtter.WayToSecureExchange.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace AnnOtter.WayToSecureExchange.Controllers
+{
+    /// <summary>
+    /// Views about the url protection
+    /// </summary>
+    public class ProtectController : Controller
+    {
+        private readonly IOptions<LabelsSettings> _labelConfig;
+        private readonly IOptions<AppearanceSettings> _appearanceConfig;
+
+        /// <summary>
+        /// Ctor of ProtectController.
+        /// </summary>
+        /// <param name="labelConfiguration">DI injected labels setting.</param>
+        /// <param name="appearanceConfiguration">DI injected appearance setting.</param>
+        public ProtectController(IOptions<LabelsSettings> labelConfiguration, IOptions<AppearanceSettings> appearanceConfiguration)
+        {
+            this._labelConfig = labelConfiguration;
+            this._appearanceConfig = appearanceConfiguration;
+        }
+
+        /// <summary>
+        /// Loads the Get-Secret View
+        /// </summary>
+        /// <returns>/Exchange/Index.cshtml</returns>
+        public IActionResult Index()
+        {
+            var viewModel = new DefaultViewModel(_labelConfig.Value, _appearanceConfig.Value);
+            return View(viewModel);
+        }
+    }
+}
